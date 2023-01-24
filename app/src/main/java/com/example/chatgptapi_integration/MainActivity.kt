03fc6af9c8cefa988bd1de6ff66aaad793dba87c
ali <md.ali2001@hotmail.com>
2023-01-25 02:25:06 +0500

    almost fetched fb profile info

--- app/src/main/java/com/example/chatgptapi_integration/MainActivity.kt
+++ app/src/main/java/com/example/chatgptapi_integration/MainActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
 import com.facebook.*
 import com.facebook.appevents.AppEventsLogger;
@@ -19,10 +21,17 @@ private fun LoginButton.registerCallback(callbackManager: CallbackManager, callb
 @Suppress("DEPRECATION")
 class MainActivity : AppCompatActivity() {
     lateinit var callbackManager: CallbackManager
+    var imageView : ImageView? =null
+
+
+
+
     private val EMAIL = "email"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val infofield: TextView=findViewById(R.id.info)
+
         val loginButton = findViewById<LoginButton>(R.id.login_button)
         loginButton.setOnClickListener {
             loginButton.setReadPermissions(listOf(EMAIL))
@@ -34,9 +43,33 @@ class MainActivity : AppCompatActivity() {
             loginButton.registerCallback(callbackManager, object : FacebookCallback<LoginResult?> {
                 override fun onSuccess(loginResult: LoginResult?) {
                     Log.d("MainActivity", "Facebook token: " + loginResult!!.accessToken.token)
-                    val graphRequest=GraphRequest.newMeRequest(loginResult.accessToken){`object` ,response->
-                        getFacebookData(`object`)
+                    val graphRequest=GraphRequest.newMeRequest(loginResult.accessToken){obj ,response->
+
+                        try{
+                            if (obj != null) {
+
+                                    val name=obj.getString("name")
+                                    val email=obj.getString("email")
+                                    infofield.text=name
+
+
+
+                            }
+                        }
+
+                        catch( e : Exception)
+                        {
+
+                        }
+                        //getFacebookData(`object`)
                     }
+
+                    val param=Bundle()
+                    param.putString("fields" , "name,email,picture.type(large)")
+                    graphRequest.parameters=param
+                    graphRequest.executeAsync()
+
+
 
                 }
                 override fun onCancel() { // App code
@@ -60,9 +93,10 @@ class MainActivity : AppCompatActivity() {
                          )
         } }
 
-    private fun getFacebookData(obj: JSONObject?) {
-        TODO("Not yet implemented")
-    }
+//    private fun getFacebookData(obj: JSONObject?) {
+//        val profilepic= "https://graph.facebook.com/${obj?.getString("id")}/picture?width=200&height=200"
+//
+//    }
 
 }
 
